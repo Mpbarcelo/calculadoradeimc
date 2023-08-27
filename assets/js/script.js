79e4b2c2c72049a4a3b43b691acd6df8148c88cd
Mpbarcelo <marciatrevinho@gmail.com>
2023-08-27 20:14:07 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const d = document;
+const form = d.getElementById("form");
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  const weight = d.getElementById("weight").value;
+  const height = d.getElementById("height").value / 100;
+
+  const bmi = (weight / (height * height)).toFixed(2);
+
+  let description = "";
+
+  value.classList.add('attention');
+
+  d.getElementById("infos").classList.remove("hidden");
+
+  if (bmi > 40) {
+    description = "Cuidado! você está com obesidade mórbida!";
+  } else if (bmi > 35) {
+    description = "Cuidado! você está com obesidade severa!";
+  } else if (bmi > 30) {
+    description = "Cuidado! você está com obesidade moderada!";
+  } else if (bmi > 25) {
+    description = "Cuidado! você está com sobrepeso!";
+  } else if (bmi > 18.5) {
+    description = "Você está no peso ideal!";
+    value.classList.remove('attention');
+    value.classList.add('normal');
+  } else {
+    description = "Cuidado! Você está abaixo do peso.";
+  }
+
+  d.getElementById("value").textContent = bmi.replace(".", ",");
+  d.getElementById("description").textContent = description;
+});
